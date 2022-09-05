1760dbdcbda6fb67658e980a0c27f6524be357a4
Michael Bond <codemonkey8510@icloud.com>
2022-09-05 00:33:16 -0400

    Let Person be observable

--- AddDadApp/Models/Person.cs
+++ AddDadApp/Models/Person.cs
@@ -1,3 +1,22 @@
 ﻿namespace AddDadApp.Models;
 
-public record Person([property: JsonPropertyName("name")] string Name, [property: JsonPropertyName("imagePath")] string ImagePath);
+[ObservableObject]
+public partial class Person
+{
+    private string name;
+    private string imagePath;
+
+    [JsonPropertyName("name")]
+    public string Name
+    {
+        get => name;
+        set => SetProperty(ref name, value);
+    }
+
+    [JsonPropertyName("imagePath")]
+    public string ImagePath
+    {
+        get => imagePath;
+        set => SetProperty(ref imagePath, value);
+    }
+}
